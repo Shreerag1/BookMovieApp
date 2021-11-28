eb0012e32ec76b0e7b4b8cf2279a1eb5554b18f6
Shreerag Marar <sreerag.kunju@gmail.com>
2021-11-28 12:42:50 +0530

    fixed login/register popup

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -169,7 +169,16 @@ const Header = (props) => {
         isOpen={openRegistrationModal}
         contentLabel="Login"
         onRequestClose={handleCloseRegisterModal}
-        className="modal"
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
       >
         <Tabs
           style={{ marginBottom: "20px" }}
